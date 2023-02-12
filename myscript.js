462b32a2590a368790c1c1bc16e464851d002271
PranavViswanathan <pranav.viswanathan11@gmail.com>
2023-02-12 17:49:06 +0530

    added gallery

--- myscript.js
+++ myscript.js
@@ -11,3 +11,32 @@ window.addEventListener('scroll', function(){
     text.style.marginRight = value * 0.5 + "px";
     btn.style.marginTop = value * 0.5 + "px";
 })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
